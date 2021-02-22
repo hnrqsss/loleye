55c7e94b5847e9df7b740a0e5a158b0dd770b092
hnrqss <hnrq.assuncao@gmail.com>
2021-02-22 20:30:49 -0300

    adicionndo configuracoesdo axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,10 +5,16 @@ const {
     REACT_APP_API_KEY
 } = process.env
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: REACT_APP_API_URL,
     headers: {
         Authorization: `Bearer ${REACT_APP_API_KEY}`
     },
     timeout: 10000,
 })
+
+api.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8'
+api.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+api.defaults.headers.post['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
+
+export { api }
